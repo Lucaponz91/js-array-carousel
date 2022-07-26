11bbf6e5cabe5a060c5460ad9132b5cd01b08e1a
Lucaponz91 <lucapo1991@msn.com>
2022-07-26 16:24:24 +0200

    add ciclo

--- js/slider.js
+++ js/slider.js
@@ -15,12 +15,27 @@ slidesWrapperEl.innerHTML = ""
 // GENERARE IL CONTENUTO DELLO SLIDER
 for (i = 0; i < slides.length; i++){
 	const src = slides[i];
+	// CREO L' LI
 	const li = document.createElement('li')
+	// AGGIUNGO LA CLASSE SLIDE ALL' LI
 	li.classList = 'slide'
+	// IMMAGINE DI PARTENZA
+	if (i === 0) {
+		li.classList.add('active')
+	}
+	// CREO IMG
 	const img = document.createElement ('img')
+	// ASSEGNO ALLA PROPRIETà SRC DI IMG IL VALORE SRC DELL'ARRAY
 	img.src = src
+	// INSERISCO IMG DENTRO LI 
+	li.append(img)
+	console.log(li)
+	// AGGIUNGO LI ALLO SLIDESWRAPPER
+	slidesWrapperEl.append(li)
 
 }
+
+// PESCO LA FRECCIA AVANTI DALL'HTML
 const nextArrowElement = document.querySelector('.arrow-next')
 nextArrowElement.addEventListener('click', function () {
 
