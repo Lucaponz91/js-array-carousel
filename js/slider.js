fe42d39e7bc4840f4c9f7e4b83dae73aa3685b23
Lucaponz91 <lucapo1991@msn.com>
2022-07-26 15:42:14 +0200

    ciclo wip

--- js/slider.js
+++ js/slider.js
@@ -7,3 +7,23 @@ const slides = [
 	'./img/04.jpg',
 	'./img/05.jpg',
 ]
+// RECUPERO LO SLIDESWRAPPERELEMENT
+
+const slidesWrapperEl = document.querySelector('.slides-wrapper')
+// LO SVUOTO
+slidesWrapperEl.innerHTML = ""
+// GENERARE IL CONTENUTO DELLO SLIDER
+for (i = 0; i < slides.length; i++){
+	const src = slides[i];
+	const li = document.createElement('li')
+	li.classList = 'slide'
+	const img = document.createElement ('img')
+	img.src = src
+
+}
+const nextArrowElement = document.querySelector('.arrow-next')
+nextArrowElement.addEventListener('click', function () {
+
+
+}
+) 
